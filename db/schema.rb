37dc244da41c636439680b8f6a1a42a02db70054
Oscar Guzman <oscar.guzman@hoytrabajas.com>
2019-08-22 14:55:55 -0500

    fix schema code

--- db/schema.rb
+++ db/schema.rb
@@ -33,12 +33,6 @@ ActiveRecord::Schema.define(version: 2019_08_22_185622) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table "nationalities", force: :cascade do |t|
-    t.string "description"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
   create_table "recruitment_types", force: :cascade do |t|
     t.string "description"
     t.datetime "created_at", precision: 6, null: false

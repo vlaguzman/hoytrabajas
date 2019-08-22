c38741e01898155941f5d4656ef1950b6cba152e
Oscar Guzman <oscar.guzman@hoytrabajas.com>
2019-08-22 14:23:33 -0500

    add timestamp

--- db/migrate/20190822185622_create_recruitment_type.rb
+++ db/migrate/20190822185622_create_recruitment_type.rb
@@ -2,6 +2,7 @@ class CreateRecruitmentType < ActiveRecord::Migration[6.0]
   def change
     create_table :recruitment_types do |t|
       t.string :description
+      t.timestamps
     end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -33,8 +33,16 @@ ActiveRecord::Schema.define(version: 2019_08_22_185622) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "nationalities", force: :cascade do |t|
+    t.string "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
   create_table "recruitment_types", force: :cascade do |t|
     t.string "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
 end

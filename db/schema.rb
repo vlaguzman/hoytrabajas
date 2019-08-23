bcdbb4c45e0aacf20b5b689ce9f15e013c913b0e
Oscar Guzman <oscar.guzman@hoytrabajas.com>
2019-08-23 11:25:09 -0500

    add job aid model

--- db/schema.rb
+++ db/schema.rb
@@ -45,6 +45,12 @@ ActiveRecord::Schema.define(version: 2019_08_23_200045) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "job_aids", force: :cascade do |t|
+    t.string "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
   create_table "job_categories", force: :cascade do |t|
     t.string "description"
     t.datetime "created_at", precision: 6, null: false
---
+++ spec/factories/job_aid.rb
@@ -0,0 +1,5 @@
+FactoryBot.define do
+  factory :job_aid do
+    description { 'Trasport aid' }
+  end
+end

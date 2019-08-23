f6f6dc0d516d88c72cfb7856b80e7c2ece16482d
Oscar Guzman <oscar.guzman@hoytrabajas.com>
2019-08-23 14:14:49 -0500

    add educational degree model

---
+++ app/models/educational_degree.rb
@@ -0,0 +1,3 @@
+class EducationalDegree < ApplicationRecord
+  validates_presence_of :description
+end
---
+++ spec/factories/educational_degree.rb
@@ -0,0 +1,5 @@
+FactoryBot.define do
+  factory :educational_degree do
+    description { 'BsicPrimary' }
+  end
+end

a75a07eb0f97dbe015ff5bfbe7093b8103723660
Gabriel Meneses <gabrielmeneses9402@gmail.com>
2019-08-23 11:05:07 -0500

    adds cities table

--- db/schema.rb
+++ db/schema.rb
@@ -15,6 +15,12 @@ ActiveRecord::Schema.define(version: 2019_08_22_214430) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "available_work_days", force: :cascade do |t|
+    t.string "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
   create_table "cities", force: :cascade do |t|
     t.string "description"
     t.datetime "created_at", precision: 6, null: false

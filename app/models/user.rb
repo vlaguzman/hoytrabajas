f6b51696302574057de5b66f3eff42acff4aef78
Gabriel Meneses <gabrielmeneses9402@gmail.com>
2019-08-26 13:47:15 -0500

    fixed syntax model and down columns provider, uid

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,5 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable,
-         :lockable, :timeoutable, :trackable
-
- devise :omniauthable, omniauth_providers:%i[facebook]
+         :lockable, :timeoutable, :trackable, :omniauthable, omniauth_providers:%i[facebook]
 end

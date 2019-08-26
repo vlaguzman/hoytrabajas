b5aedfc2b456708e76680c3f10312c235ec98b5e
Oscar Guzman <oscar.guzman@hoytrabajas.com>
2019-08-26 14:48:26 -0500

    add vehicle model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_26_191752) do
+ActiveRecord::Schema.define(version: 2019_08_26_194614) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -161,6 +161,12 @@ ActiveRecord::Schema.define(version: 2019_08_26_191752) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["unlock_token"], name: "index_users_on_unlock_token", unique: true
+  end
+
+  create_table "vehicles", force: :cascade do |t|
+    t.string "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "work_positions", force: :cascade do |t|
---
+++ spec/factories/vehicle.rb
@@ -0,0 +1,5 @@
+FactoryBot.define do
+  factory :vehicle do
+    description { 'Motorcycle' }
+  end
+end

c75ef0e422e588dfd036517f1b876362b400768e
Gabriel Meneses <gabrielmeneses9402@gmail.com>
2019-08-26 22:10:04 -0500

    adds first implementation for users profile

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   devise_for :companies
   devise_for :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  namespace :users do
+    resource :profile, only: [:create, :update, :show]
+  end
   root to: "home#index"
 end
--- spec/features/fill_candidate_user_data_spec.rb
+++ spec/features/fill_candidate_user_data_spec.rb
@@ -2,9 +2,23 @@ require 'rails_helper'
 
 RSpec.describe "fill the principal canditate user data", :type => :feature do
   context "like a candidate user in his first login" do
-    xit "should see the info to fill and the next button in each section" do
-      expect(User.count).to eq 0
-      
+    it "should see the info to fill and the next button in each section" do
+
+      # a new user who has never been loged-in, i.e. sign_in_count is zero
+      user = User.create(email: 'nuevousuario@gmail.com', password: 'hola12345', password_confirmation: 'hola12345', sign_in_count: 0, confirmed_at: Date.today)
+
+      visit new_user_session_path
+
+      within '#new_user' do
+        fill_in 'user_email', with: 'nuevousuario@gmail.com'
+        fill_in 'user_password', with: 'hola12345'
+        click_on 'Log in'
+      end
+
+      # dado el usuario se logea por primera vez, es redirigido a la pagina
+      # de registr ode informacion
+      # TODO: hacer otra prueba para rediririr al usuraia a otra pantalla
+      # cuando ya ha sido logueado antes
       expect(page).to have_text("Empecemos por conocernos")
       fill_in "name", :with => "Carlos"
       fill_in "last_name", :with => "Rojas"

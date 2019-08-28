f37b53d3f799278201d4f9417227d2eb70b89268
Oscar Guzman <oscar.guzman@hoytrabajas.com>
2019-08-28 14:26:17 -0500

    recreate the schema after rebase

--- db/schema.rb
+++ db/schema.rb
@@ -23,6 +23,27 @@ ActiveRecord::Schema.define(version: 2019_08_29_144815) do
     t.index ["curriculum_vitae_id"], name: "index_acknowledgments_on_curriculum_vitae_id"
   end
 
+  create_table "active_storage_attachments", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "record_type", null: false
+    t.bigint "record_id", null: false
+    t.bigint "blob_id", null: false
+    t.datetime "created_at", null: false
+    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
+    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
+  end
+
+  create_table "active_storage_blobs", force: :cascade do |t|
+    t.string "key", null: false
+    t.string "filename", null: false
+    t.string "content_type"
+    t.text "metadata"
+    t.bigint "byte_size", null: false
+    t.string "checksum", null: false
+    t.datetime "created_at", null: false
+    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
   create_table "available_work_days", force: :cascade do |t|
     t.string "description"
     t.datetime "created_at", precision: 6, null: false
@@ -291,6 +312,7 @@ ActiveRecord::Schema.define(version: 2019_08_29_144815) do
   end
 
   add_foreign_key "acknowledgments", "curriculum_vitaes"
+  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "curriculum_vitaes_soft_skills", "curriculum_vitaes"
   add_foreign_key "curriculum_vitaes_soft_skills", "soft_skills"
   add_foreign_key "recommendations", "curriculum_vitaes"

b576c299f72f8239de2176e3c741d6689d37969f
Oscar Guzman <oscar.guzman@hoytrabajas.com>
2019-08-31 11:09:28 -0500

    own review #1

--- spec/factories/offer.rb
+++ spec/factories/offer.rb
@@ -1,19 +1,19 @@
 FactoryBot.define do
   factory :offer do
-      title                         { "Ruby on Rails developer with 10 years of experience" }
-      address                       { "Fake Street 123 # 99 - 01" }
-      cellphone                     { "3002004050" }
-      description                   { "endSint esse anim consequat commodo." }
-      vacancies_quantity            { 10 }
-      close_date                    { Date.new + 1.month }
-      immediate_start               { true }
-      required_experience           { true }
-      description_responsibilities  { "endSint esse anim consequat commodo." }
-      release_date                  { Date.new }
-      status                        { "published" }
+    title                         { "Ruby on Rails developer with 10 years of experience" }
+    address                       { "Fake Street 123 # 99 - 01" }
+    cellphone                     { "3002004050" }
+    description                   { "endSint esse anim consequat commodo." }
+    vacancies_quantity            { 10 }
+    close_date                    { Date.new + 1.month }
+    immediate_start               { true }
+    required_experience           { true }
+    description_responsibilities  { "endSint esse anim consequat commodo." }
+    release_date                  { Date.new }
+    status                        { "published" }
 
-      after(:build) do |offer|
-        offer.image.attach(io: File.open(Rails.root.join('spec', 'factories', 'images', 'photo.jpg')), filename: 'photo.jpg', content_type: 'image/jpeg')
-      end
+    after(:build) do |offer|
+      offer.image.attach(io: File.open(Rails.root.join('spec', 'factories', 'images', 'photo.jpg')), filename: 'photo.jpg', content_type: 'image/jpeg')
+    end
   end
 end

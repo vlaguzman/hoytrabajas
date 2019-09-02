b12c768c99bcb4b0801ab3d711dbadc235caa877
Oscar Guzman <oscar.guzman@hoytrabajas.com>
2019-09-02 17:17:45 -0500

    add offers salaries assocs

---
+++ spec/models/offers_salaries_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe OffersSalaries, type: :model do
+
+  describe "validations" do
+    it { should validate_presence_of(:from) }
+    it { should validate_presence_of(:to) }
+  end
+
+  describe "associations" do
+    it { should belong_to(:currency) }
+    it { should belong_to(:salary_type) }
+    it { should belong_to(:salary_period) }
+    it { should belong_to(:offer) }
+  end
+end

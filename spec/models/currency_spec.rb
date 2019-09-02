3c98cfd3d48c0ad00c36136e4ceee8d5c471f397
Oscar Guzman <oscar.guzman@hoytrabajas.com>
2019-09-02 16:50:28 -0500

    add curriculum vitae and salary assoc

--- spec/models/currency_spec.rb
+++ spec/models/currency_spec.rb
@@ -5,10 +5,4 @@ RSpec.describe Currency, type: :model do
   describe "validations" do
     it  { should validate_presence_of(:description) }
   end
-
-
-  describe 'associations' do
-    #it { should have_and_belong_to_many(:offers) }
-    #it { should have_and_belong_to_many(:curriculum_vitaes) }
-  end
 end
--- spec/models/salary_period_spec.rb
+++ spec/models/salary_period_spec.rb
@@ -5,9 +5,4 @@ RSpec.describe SalaryPeriod, type: :model do
   describe "validations" do
     it { should validate_presence_of(:description) }
   end
-
-  describe "associoations" do
-    #it { should have_and_belong_to_many(:curriculum_vitaes) }
-    #it { should have_and_belong_to_many(:offers) }
-  end
 end

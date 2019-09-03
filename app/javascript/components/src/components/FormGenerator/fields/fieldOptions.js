50dc27e18d65be9f31d4254624837e81ab6ee4ca
Javier Segovia <javier.segovia@hoytrabajas.com>
2019-09-03 15:18:14 -0400

    fix fieldOptions

--- app/javascript/components/src/components/FormGenerator/fields/fieldOptions.js
+++ app/javascript/components/src/components/FormGenerator/fields/fieldOptions.js
@@ -1,27 +1,27 @@
 // Fields
 
-import Button from '../components/FormGenerator/fields/Button'
-import Checkbox from '../components/FormGenerator/fields/Checkbox'
-import Chips from '../components/FormGenerator/fields/Chips'
+import Button from '../../../components/FormGenerator/fields/Button'
+import Checkbox from '../../../components/FormGenerator/fields/Checkbox'
+import Chips from '../../../components/FormGenerator/fields/Chips'
 import ControlledInputs from '../components/FormGenerator/fields/ControlledInputs'
-import DatePicker from '../components/FormGenerator/fields/DatePicker'
-import DragNDrop from '../components/FormGenerator/fields/DragNDrop'
-import FourButtons from '../components/FormGenerator/fields/FourButtons'
-import MultiSelect from '../components/FormGenerator/fields/MultiSelect'
-import Radios from '../components/FormGenerator/fields/Radios'
-import ResponsiveDialog from '../components/FormGenerator/fields/ResponsiveDialog'
-import Select from '../components/FormGenerator/fields/Select'
-import SelectChip from '../components/FormGenerator/fields/SelectChip'
-import SkipButton from '../components/FormGenerator/fields/SkipButton'
-import Slider from '../components/FormGenerator/fields/Slider'
-import StandardInput from '../components/FormGenerator/fields/StandardInput'
-import Switch from '../components/FormGenerator/fields/Switch'
-import TextChip from '../components/FormGenerator/fields/TextChip'
+import DatePicker from '../../../components/FormGenerator/fields/DatePicker'
+import DragNDrop from '../../../components/FormGenerator/fields/DragNDrop'
+import FourButtons from '../../../components/FormGenerator/fields/FourButtons'
+import MultiSelect from '../../../components/FormGenerator/fields/MultiSelect'
+import Radios from '../../../components/FormGenerator/fields/Radios'
+import ResponsiveDialog from '../../../components/FormGenerator/fields/ResponsiveDialog'
+import Select from '../../../components/FormGenerator/fields/Select'
+import SelectChip from '../../../components/FormGenerator/fields/SelectChip'
+import SkipButton from '../../../components/FormGenerator/fields/SkipButton'
+import Slider from '../../../components/FormGenerator/fields/Slider'
+import StandardInput from '../../../components/FormGenerator/fields/StandardInput'
+import Switch from '../../../components/FormGenerator/fields/Switch'
+import TextChip from '../../../components/FormGenerator/fields/TextChip'
 
 // Wrappers
 
-import SimpleRowWrapper from '../components/FormGenerator/wrappers/SimpleRowWrapper'
-import FormRowWrapper from '../components/FormGenerator/wrappers/FormRowWrapper'
+import SimpleRowWrapper from '../../../components/FormGenerator/wrappers/SimpleRowWrapper'
+import FormRowWrapper from '../../../components/FormGenerator/wrappers/FormRowWrapper'
 
 export default {
   button: props => <Button {...props} />,

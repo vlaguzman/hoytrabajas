bf835952378e0e27066ce498d24323ae1a5a2c8c
Javier Segovia <javier.segovia@hoytrabajas.com>
2019-09-03 17:18:04 -0400

    fix route primaryTheme

--- app/javascript/components/src/components/Layout/Theme/primaryTheme.js
+++ app/javascript/components/src/components/Layout/Theme/primaryTheme.js
@@ -1,5 +1,5 @@
 import { createMuiTheme } from '@material-ui/core/styles'
-import AppConfig from '../constants/AppConfig'
+import AppConfig from '../../../constants/AppConfig'
 
 const theme = createMuiTheme({
   palette: {

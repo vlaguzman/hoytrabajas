a022fd4dd710436ee7fdb18a4f3f27b48727c30c
Oscar Guzman <oscar.guzman@hoytrabajas.com>
2019-09-03 10:26:38 -0500

    active all device fields of company

--- db/schema.rb
+++ db/schema.rb
@@ -121,11 +121,25 @@ ActiveRecord::Schema.define(version: 2019_09_03_002841) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.inet "current_sign_in_ip"
+    t.inet "last_sign_in_ip"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
+    t.integer "failed_attempts", default: 0, null: false
+    t.string "unlock_token"
+    t.datetime "locked_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["confirmation_token"], name: "index_companies_on_confirmation_token", unique: true
     t.index ["email"], name: "index_companies_on_email", unique: true
     t.index ["employees_range_id"], name: "index_companies_on_employees_range_id"
     t.index ["reset_password_token"], name: "index_companies_on_reset_password_token", unique: true
+    t.index ["unlock_token"], name: "index_companies_on_unlock_token", unique: true
   end
 
   create_table "companies_users", force: :cascade do |t|

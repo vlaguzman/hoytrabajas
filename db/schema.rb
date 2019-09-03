6a02539edd53e23ddfb3939c62641431a00a4239
Oscar Guzman <oscar.guzman@hoytrabajas.com>
2019-09-03 14:16:39 -0500

    add timestamps in terms model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_03_170738) do
+ActiveRecord::Schema.define(version: 2019_09_03_190629) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -613,6 +613,8 @@ ActiveRecord::Schema.define(version: 2019_09_03_170738) do
 
   create_table "terms", force: :cascade do |t|
     t.string "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "users", force: :cascade do |t|

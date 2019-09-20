31144101bb01d9d76ed3689591c53000bb35f5d1
Gabriel Meneses <gabrielmeneses9402@gmail.com>
2019-09-19 19:13:14 -0500

    try the fix problem with react component gallery, in progress

--- app/javascript/components/src/views/main/home/sections/gallery.js
+++ app/javascript/components/src/views/main/home/sections/gallery.js
@@ -4,36 +4,31 @@ import Button from '@material-ui/core/Button';
 // import IntlMessages from 'Util/IntlMessages'
 import Cards from './components/gallery_cards/gallery_card'
 import Carousel from '../../../../components/Carousel/CarouselRow'
-import { map } from 'lodash';
 
+const ofertas = Array(6).fill(null)
 
-export default ({
-  children,
-  ofertas,
-  button,
-  title: { main, highlighted, last }
-}) => {
+export default () => {
   return (
     <div className="">
       <div className="sec-title mt-20 mb-40 mx-10  text-center">
-        {main}&nbsp;<span className="text-primary">{highlighted}</span>&nbsp;
-        {last}
+        el trabajo ideal &nbsp;<span className="text-primary">si existe</span>&nbsp;
+        esta en hoy trabajas
       </div>
       <Carousel slidesToShowResp={1} centerMode={true} infinite={true} slidesToShow={3.7} autoplay={false} >
-      {map(ofertas,(e, i) => (
-        <Cards key={i} />
-      ))}
+      ofertas.map((e, i) => (
+        <Cards key = {i} />
+      ))
       </Carousel>
       <Row className="justify-content-center align-items-center  my-30 ">
         <Col xs={9} md={4}>
           <Button
-          color='primary'
+            color='primary'
             size="large"
             variant='outlined'
             className="my-20 text-wrap"
             style={{ borderRadius: '50px' }}
           >
-            {button}
+            ver más ofertas
           </Button>
         </Col>
       </Row>

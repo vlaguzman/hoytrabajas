26861a64ded90ec0c7562b5027b49fdfd59d3225
Gabriel Meneses <gabrielmeneses9402@gmail.com>
2019-09-19 23:19:43 -0500

    offer component adding in views

--- app/javascript/components/src/views/main/home/sections/gallery.js
+++ app/javascript/components/src/views/main/home/sections/gallery.js
@@ -8,16 +8,27 @@ import Carousel from '../../../../components/Carousel/CarouselRow'
 const ofertas = Array(6).fill(null)
 
 export default () => {
+  // listast: map, filter, reduce
+  // map (list, f(x)) -> list
+  // lista = [2, 3, 7], f(x){x+1}     map(lista, f)->[3, 4, 8]
+  // filter (lista, f) -> lista   f(x){x>4} filtre(lita, f)->[7]
+  // reduce (lista, f) -> escalar sum, max, min, avg
+  //reduce(lista, sum) -> 12
+  //reduce(lista, min) -> 2
+  //reduce(lista, pares_sumados) -> [5, 10]
+
+  let lasOfertas = ofertas.map((e, i) => {
+       return (  <Cards key = {i} /> )
+      })
   return (
     <div className="">
+      <h1>Offers</h1>
       <div className="sec-title mt-20 mb-40 mx-10  text-center">
         el trabajo ideal &nbsp;<span className="text-primary">si existe</span>&nbsp;
         esta en hoy trabajas
       </div>
       <Carousel slidesToShowResp={1} centerMode={true} infinite={true} slidesToShow={3.7} autoplay={false} >
-      ofertas.map((e, i) => (
-        <Cards key = {i} />
-      ))
+        {lasOfertas}
       </Carousel>
       <Row className="justify-content-center align-items-center  my-30 ">
         <Col xs={9} md={4}>

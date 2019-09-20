fe4445ae1fc52689a2db1767236a6a520e22f142
Gabriel Meneses <gabrielmeneses9402@gmail.com>
2019-09-20 09:28:05 -0500

    delete lines comment

--- app/javascript/components/src/views/main/home/sections/gallery.js
+++ app/javascript/components/src/views/main/home/sections/gallery.js
@@ -8,14 +8,6 @@ import Carousel from '../../../../components/Carousel/CarouselRow'
 const ofertas = Array(6).fill(null)
 
 export default () => {
-  // listast: map, filter, reduce
-  // map (list, f(x)) -> list
-  // lista = [2, 3, 7], f(x){x+1}     map(lista, f)->[3, 4, 8]
-  // filter (lista, f) -> lista   f(x){x>4} filtre(lita, f)->[7]
-  // reduce (lista, f) -> escalar sum, max, min, avg
-  //reduce(lista, sum) -> 12
-  //reduce(lista, min) -> 2
-  //reduce(lista, pares_sumados) -> [5, 10]
 
   let lasOfertas = ofertas.map((e, i) => {
        return (  <Cards key = {i} /> )

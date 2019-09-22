4778cf2eb2ca06af49be96cfe0611c7e1ac89c2f
Julian Vargas <julianvargasalvarez@gmail.com>
2019-09-21 21:13:20 -0500

    renders full app layout

--- app/javascript/components/Micomponente.js
+++ app/javascript/components/Micomponente.js
@@ -6,24 +6,20 @@ import { es, enUS as en } from 'date-fns/locale'
 import store from './src/store'
 import CssBaseline from '@material-ui/core/CssBaseline'
 import AppLayout from './src/components/Layout/AppLayout'
-//import 'slick-carousel/slick/slick.css'
+import 'slick-carousel/slick/slick.css'
+import 'slick-carousel/slick/slick-theme.css'
+import './assets/static/themify-icons/themify-icons.css'
+import './assets/static/scss/_style.scss'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+import { fas, faSearch } from '@fortawesome/free-solid-svg-icons'
 
+library.add(fab, fas, faSearch)
 
-
-//import 'slick-carousel/slick/slick-theme.css'
-//import './assets/static/themify-icons/themify-icons.css'
-//import './assets/static/scss/_style.scss'
-//import { library } from '@fortawesome/fontawesome-svg-core'
-//import { fab } from '@fortawesome/free-brands-svg-icons'
-//import { fas, faSearch } from '@fortawesome/free-solid-svg-icons'
-
-//library.add(fab, fas, faSearch)
-
-//const localeMap = { en, es }
-//const locale = 'es'
+const localeMap = { en, es }
+const locale = 'es'
 
 export default class Micomponente extends React.Component {
-/*
   componentDidMount() {
     // Remove the server-side injected CSS.
     const jssStyles = document.querySelector('#jss-server-side')
@@ -31,10 +27,7 @@ export default class Micomponente extends React.Component {
       jssStyles.parentNode.removeChild(jssStyles)
     }
   }
-*/
   render() {
-    return (<AppLayout><CssBaseline /></AppLayout>)
-/*
     return (
           <MuiPickersUtilsProvider
             utils={DateFnsUtils}
@@ -45,7 +38,7 @@ export default class Micomponente extends React.Component {
               </AppLayout>
           </MuiPickersUtilsProvider>
     )
-*/
+
   }
 }
 

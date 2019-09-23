b8190958f95366bb3ceeb9d949a78c6f30630f88
Gabriel Meneses <gabrielmeneses9402@gmail.com>
2019-09-22 22:24:52 -0500

    Scrollbar commented, search possible solutions

--- app/javascript/components/src/components/Layout/AppLayout.js
+++ app/javascript/components/src/components/Layout/AppLayout.js
@@ -1,5 +1,5 @@
 import React, { useState, useRef } from 'react'
-import { Scrollbars } from 'react-custom-scrollbars'
+//import { Scrollbars } from 'react-custom-scrollbars'
 import {
   ThemeProvider as MuiThemeProvider,
   StylesProvider
@@ -13,14 +13,14 @@ import theme from '../../components/Layout/Theme/theme'
 const AppLayout = ({ children }) => {
   const [isTop, setIsTop] = useState(false)
   const myRef = useRef(null)
-  const onScrollFrame = e => {
+  /*const onScrollFrame = e => {
     const currentTop = e.scrollTop > 200
     if (currentTop !== isTop) setIsTop(currentTop)
   }
   const scrollToBottom = () => {
     console.log('las barras',myRef)
     myRef.current.scrollToBottom()
-  }
+  }*/
   return ( 
     <StylesProvider injectFirst>
       <MuiThemeProvider theme={theme}>
@@ -31,7 +31,7 @@ const AppLayout = ({ children }) => {
                 <div className="app-container">
                   <div className="rct-app-content">
                     <div className="rct-page" style={{ background: 'white' }}>
-                      <Scrollbars
+                     /* <Scrollbars
                         className="rct-scroll"
                         onScrollFrame={onScrollFrame}
                         universal
@@ -47,7 +47,7 @@ const AppLayout = ({ children }) => {
                           {children}
                           <Footer {...{scrollToBottom} }/>
                         </div>
-                      </Scrollbars>
+                      </Scrollbars>*/
                     </div>
                   </div>
                 </div>

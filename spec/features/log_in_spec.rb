b6c5295d53aa46ac519250561c9966996038a507
Gabriel Meneses <gabrielmeneses9402@gmail.com>
2019-09-30 16:12:20 -0500

    corrections made

--- spec/features/log_in_spec.rb
+++ spec/features/log_in_spec.rb
@@ -1,19 +1,21 @@
 require 'rails_helper'
 
-RSpec.describe "User can Log In" do
+RSpec.describe "User can Log In", type: :feature do
 
   before {
     create(:user, email: "example@email.com", password: "Asdf1234")  
   }
 
   context "a registered user must be able to login" do
-    scenario "redirect after login", js: true, type: :feature do
+    scenario "redirect after login", js: true do
 
       visit root_path
 
       expect(page).to have_content("Login")
+
       has_button?("Login")
       click_on("Login")
+
       within "#new_user" do
         fill_in "user_email", with: 'example@email.com'
         fill_in "user_password", with: 'Asdf1234'

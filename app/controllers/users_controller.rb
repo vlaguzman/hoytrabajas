a553e7acaa0bb826cb0566ecb6eb715896d147a9
Gabriel Meneses <gabrielmeneses9402@gmail.com>
2019-10-01 15:27:26 -0500

    redirect after log in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,13 +3,13 @@ class UsersController < ApplicationController
   before_action :authenticate_user!
 
   def edit
-    first_sign_in?(current_user, users_wizards_step_zero_path)
   end
 
   def update
   end
 
   def show
+    first_sign_in?(current_user, user_path)
   end
 
 end

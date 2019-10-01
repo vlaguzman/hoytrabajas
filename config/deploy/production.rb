aa27e288ec588685bc4b3885e476608170aa2220
Julian Vargas <julianvargasalvarez@gmail.com>
2019-09-30 23:31:27 -0500

    removes reload

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,7 +1,1 @@
 server "ec2-35-170-202-220.compute-1.amazonaws.com", user: "deploy", roles: %w{app db web}
-
-namespace :deploy do
-  after :finishing, :notify do
-    `ssh deploy@ec2-35-170-202-220.compute-1.amazonaws.com "cd /var/www/ht-ror/current/; ./bin/reload"`
-  end
-end

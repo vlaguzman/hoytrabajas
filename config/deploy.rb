eeca11d36a257aaf94d03cec7c84c3ab6ce26de5
Julian Vargas <julianvargasalvarez@gmail.com>
2019-10-01 20:46:56 -0500

    adds reload

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,6 +5,5 @@ set :repo_url, "git@bitbucket.org:vladimir_ht/ht-ror.git"
 append :linked_files, "config/database.yml", "config/secrets.yml"
 append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system", "public/assets", "public/uploads", "node_modules"
 
-set :rvm_type, :user                     # Defaults to: :auto
-set :rvm_ruby_version, '2.6.3'      # Defaults to: 'default'
-
+set :rvm_type, :user
+set :rvm_ruby_version, '2.6.3'
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,1 +1,15 @@
 server "ec2-35-170-202-220.compute-1.amazonaws.com", user: "deploy", roles: %w{app db web}
+
+namespace :deploy do
+  after :finishing, :notify do
+    on roles(:app) do
+      within current_path do
+        as :deploy  do
+          with rails_env: :production do
+            execute "bin/reload", :shell => fetch(:rvm_shell)
+          end
+        end
+      end
+    end
+  end
+end

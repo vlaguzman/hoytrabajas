41e1bbd44d15308718c6ec65ef6b11e38cf0c8e8
Gabriel Meneses <gabrielmeneses9402@gmail.com>
2019-10-05 12:32:00 -0500

    fix css for registration

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -32,12 +32,26 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Don't care if the mailer can't send.
+  # Action mailer
   config.action_mailer.raise_delivery_errors = true 
   config.action_mailer.delivery_method = :test
   host = 'localhost:3000'
   config.action_mailer.default_url_options = { host: host, protocol: 'http'}
 
   config.action_mailer.perform_caching = false
+
+  # smpt_settings
+
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'gmail.com',
+    user_name:            'gabrielmeneses9402@gmail.com',
+    password:             'k7aws43bej89',
+    authentication:       :plain,
+    enable_starttls_auto: true
+  }
+
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log

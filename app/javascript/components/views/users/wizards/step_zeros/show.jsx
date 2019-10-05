a0f49e97cfe7ba3ac55f50655f8d4cdc7af07d8b
Julian Vargas <julianvargasalvarez@gmail.com>
2019-10-05 15:33:20 -0500

    fixes layout

--- app/javascript/components/views/users/wizards/step_zeros/show.jsx
+++ app/javascript/components/views/users/wizards/step_zeros/show.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
-export default class Show extends React.Component {
+import AppLayout from '../../../../src/components/Layout/AppLayout'
 
+export default class Show extends React.Component {
   render() {
     return (
-      <div>
-        <h1>El trabajo ideal</h1>
-        <h1>¡si existe!</h1>
-        <a href="#">Quiero públicar mi oferta</a>
+      <div className="main-wrapper">
+        <AppLayout>
+          <h1>El trabajo ideal</h1>
+          <h1>¡si existe!</h1>
+          <a href="#">Quiero públicar mi oferta</a>
+        </AppLayout>
       </div>
     )
   }

4b3038ee1f50d497e5960385e027b60d1a947083
Julian Vargas <julianvargasalvarez@gmail.com>
2019-10-05 15:24:51 -0500

    adds react for wizard step zero

---
+++ app/javascript/components/views/users/wizards/step_zeros/show.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+export default class Show extends React.Component {
+
+  render() {
+    return (
+      <div>
+        <h1>El trabajo ideal</h1>
+        <h1>¡si existe!</h1>
+        <a href="#">Quiero públicar mi oferta</a>
+      </div>
+    )
+  }
+}

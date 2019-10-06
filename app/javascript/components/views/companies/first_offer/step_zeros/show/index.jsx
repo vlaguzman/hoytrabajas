a6f34e0c31d20cd5cbbddcd44e89341d8b8e1ade
Julian Vargas <julianvargasalvarez@gmail.com>
2019-10-06 00:18:01 -0500

    adds a separate component for companies step zero

---
+++ app/javascript/components/views/companies/first_offer/step_zeros/show/index.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import AppLayout from '../../../../../src/components/Layout/AppLayout'
+
+export default class Show extends React.Component {
+  constructor(props) {
+    super(props)
+    this.props = props
+  }
+  render() {
+    return (
+      <div className="main-wrapper">
+        <AppLayout>
+          <h1>El empleado ideal</h1>
+          <h1>¡si existe!</h1>
+          <p>Y crear una oferta atractiva para llegar</p>
+          <p>a el es muy fácil ¡Vamos a hacerlo!</p>
+
+          <a href={this.props.companies_first_offer_step_one_path}>Quiero publicar mi oferta</a>
+        </AppLayout>
+      </div>
+    )
+  }
+}
+

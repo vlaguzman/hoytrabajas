509bfdfd7fbe44902c23261572fc05d3e1bf4d63
Javier Segovia <javier.segovia@hoytrabajas.com>
2019-10-07 12:26:27 -0400

    refactor of step_zero (users)

--- app/javascript/components/src/views/regcan/index.js
+++ app/javascript/components/src/views/regcan/index.js
@@ -1,21 +1,15 @@
-import React, {Fragmemt} from 'react'
-import Head from 'next/head'
-import { useRouter } from 'next/router'
+import React from 'react'
 import Cortinilla from './shared/Cortinilla'
 import data from './data'
 
 const RegPage = () => {
 
   return (
-    <Fragment>
-      <Head>
-        <title>Registro de candidato</title>
-        <meta name="description" content="Formularios de HoyTrabajas" />
-      </Head>
+    <>
       <div className="d-flex my-50 justify-content-center w-100 pb-50 ">
         <Cortinilla redirectTo="/regcan/forms" {...data[0]} />
       </div>
-    </Fragment>
+    </>
   )
 }
 

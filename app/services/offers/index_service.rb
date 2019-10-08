ba28ed5d369d36371107ab485400a92a44e64e55
Vladimir Guzman <guzman.vla@gmail.com>
2019-10-08 23:23:21 +0000

    new flag at home

--- app/services/offers/index_service.rb
+++ app/services/offers/index_service.rb
@@ -6,4 +6,14 @@ class Offers::IndexService < Offers::ViewsService
 
   private
 
+  def build_details
+    {
+      city:                 { description: offer.city_description },
+      salary:               salary_details,
+      company:              company_details,
+      close_date:           DatesConverter.default(date: offer.close_date),
+      new_offer:            offer.created_at > 24.hours.ago
+    }
+  end
+
 end

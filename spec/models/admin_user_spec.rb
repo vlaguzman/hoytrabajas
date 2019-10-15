546cd1cf41ce9a7d4bc8cbcd0c943272bf08ba6f
Vladimir Guzman <guzman.vla@gmail.com>
2019-10-15 17:18:32 +0000

    spec admin user

--- spec/models/admin_user_spec.rb
+++ spec/models/admin_user_spec.rb
@@ -1,8 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe AdminUser, type: :model do
+
   describe "validations" do
     it { should validate_presence_of(:email) }
     it { should validate_presence_of(:password) }
   end
+
 end

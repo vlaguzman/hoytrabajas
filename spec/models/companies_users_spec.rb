8747678e73f232f4455a48f8a8fa0f5f22c0bd6a
DanielSantiagoCampos <camposdaniel1002@gmail.com>
2019-10-15 15:10:09 -0500

    fix test

--- spec/models/companies_users_spec.rb
+++ spec/models/companies_users_spec.rb
@@ -5,6 +5,5 @@ RSpec.describe CompaniesUsers, type: :model do
   describe "associations" do
     it { should belong_to(:company) }
     it { should belong_to(:user) }
-    it { should belong_to(:industry) }
   end
 end

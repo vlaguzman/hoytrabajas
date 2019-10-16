db8f34db9176059267ecc3c015e08dda310f45fb
Vladimir Guzman <guzman.vla@gmail.com>
2019-10-16 17:29:57 +0000

    test to create_new_user in admin

--- spec/features/create_new_user_spec.rb
+++ spec/features/create_new_user_spec.rb
@@ -23,6 +23,21 @@ RSpec.describe "Admin can create an User", type: :feature do
       click_on("Añadir")
 
       expect(page).to have_content("Añadir User")
+
+      within '#new_user' do
+        fill_in 'user_name', with: 'roberto'
+        fill_in 'user_last_name', with: 'carlos'
+        fill_in 'user_email', with: 'carlosr@gmail.com'
+        fill_in 'user_password', with: 'jogabonito'
+        fill_in 'user_password_confirmation', with: 'jogabonito'
+        click_on 'Guardar User'
+      end
+
+      expect(page).to have_content("Detalles de User")
+      expect(page).to have_content("carlosr@gmail.com")
+
+      expect(User.count).to eq(1)
+      expect(User.last.email).to eq("carlosr@gmail.com")
     end
   end
 end

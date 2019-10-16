c496e00fc5eb9413283bdb85b48c5ad65c69a65b
Vladimir Guzman <guzman.vla@gmail.com>
2019-10-16 14:50:50 +0000

    test created

---
+++ spec/features/create_new_user_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+RSpec.describe "Admin can create an User", type: :feature do
+  before {
+    create(:admin_user, email: "admin@email.com", password: "admin1234")
+  }
+  context "a admin user must be able to create a new user" do
+    scenario "the admin fill all the data" do
+
+      visit new_admin_user_session_path
+      expect(page).to have_content("Log in")
+      within "#new_admin_user" do
+        fill_in "admin_user_email", with: 'admin@email.com'
+        fill_in "admin_user_password", with: 'admin1234'
+        has_button?("Log in")
+        click_on("Log in")
+      end
+      expect(page).to have_content("Active Admin")
+
+      has_button?("Users")
+      click_on("Users")
+      has_button?("Añadir")
+      click_on("Añadir")
+
+    end
+  end
+end

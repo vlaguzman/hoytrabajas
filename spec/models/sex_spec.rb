b170453d9343ae299b280a9bd114f5115a7e1dc2
Julian Vargas <julianvargasalvarez@gmail.com>
2019-10-17 10:02:14 -0500

    moves code for initializing SimpleCove  to the top of rails_helper

--- spec/models/sex_spec.rb
+++ spec/models/sex_spec.rb
@@ -1,10 +1,5 @@
 require 'rails_helper'
 
 RSpec.describe Sex, type: :model do
-  it "should validate the presence of description" do
-    sex = FactoryBot.build(:sex, description:nil)
-    sex.valid?
-
-    expect(sex.errors[:description].size).to eq(1) 
-  end
+  it { should validate_presence_of :description }
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,10 +1,3 @@
-# This file is copied to spec/ when you run 'rails generate rspec:install'
-require 'spec_helper'
-ENV['RAILS_ENV'] ||= 'test'
-require File.expand_path('../../config/environment', __FILE__)
-# Prevent database truncation if the environment is production
-abort("The Rails environment is running in production mode!") if Rails.env.production?
-require 'rspec/rails'
 require "simplecov"
 require "simplecov-csv"
 SimpleCov.formatters = SimpleCov::Formatter::MultiFormatter.new([
@@ -19,6 +12,13 @@ SimpleCov.start 'rails' do
   add_filter "/app/controllers/"
   add_filter "/app/admin/"
 end
+# This file is copied to spec/ when you run 'rails generate rspec:install'
+require 'spec_helper'
+ENV['RAILS_ENV'] ||= 'test'
+require File.expand_path('../../config/environment', __FILE__)
+# Prevent database truncation if the environment is production
+abort("The Rails environment is running in production mode!") if Rails.env.production?
+require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in

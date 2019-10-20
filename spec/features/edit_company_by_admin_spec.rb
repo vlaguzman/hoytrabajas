d9c2a161135f005b21e8ba1215d77f8ac9b1c337
Vladimir Guzman <guzman.vla@gmail.com>
2019-10-20 04:10:05 +0000

    edit_company_by_admin_spec modified

--- spec/features/edit_company_by_admin_spec.rb
+++ spec/features/edit_company_by_admin_spec.rb
@@ -1,40 +1,38 @@
 require 'rails_helper'
 
-RSpec.describe "Admin can edit an User", type: :feature do
+RSpec.describe "Admin can edit an Company", type: :feature do
   include Devise::Test::IntegrationHelpers
 
-  context "a admin user must be able to edit a created user" do
+  context "a admin user must be able to edit a created company" do
     
-    let!(:user) { create(:user, name: 'carlos', email: 'elcarlos@gmail.com') }
+    let!(:company) { create(:company, name: 'rocknroll', email: 'rocknroll@company.com') }
 
-    scenario "the admin select an user and edit all the data" do
+    scenario "the admin select an company and edit all the data" do
       
       sign_in FactoryBot.create(:admin_user)
       visit admin_dashboard_path 
       expect(page).to have_content("Active Admin")
 
-      has_button?("Users")
-      click_on("Users")
+      has_button?("Companies")
+      click_on("Companies")
 
-      expect(page).to have_content("elcarlos@gmail.com")
+      expect(page).to have_content("rocknroll@company.com")
       has_button?("Editar")
       click_on("Editar")
-      expect(page).to have_content("Editar User")
-      expect(page).to have_content("Email")
+      expect(page).to have_content("Editar Company")
 
-      within '#edit_user' do
-        fill_in 'user_name', with: 'roberto'
-        fill_in 'user_last_name', with: 'carlos'
-        fill_in 'user_email', with: 'carlosr@gmail.com'
-        fill_in 'user_password', with: 'jogabonito'
-        fill_in 'user_password_confirmation', with: 'jogabonito'
-        click_on("Guardar User")
+      within '#edit_company' do
+        fill_in 'company_name', with: 'rootnpool'
+        fill_in 'company_email', with: 'rootnpool@company.com'
+        fill_in 'company_password', with: 'ontherocks'
+        fill_in 'company_password_confirmation', with: 'ontherocks'
+        click_on("Guardar Company")
       end
 
-      expect(page).to have_content("Detalles de User")
-      expect(page).to have_content("carlosr@gmail.com")
+      expect(page).to have_content("Detalles de Company")
+      expect(page).to have_content("rootnpool@company.com")
 
-      expect(User.last.email).to eq("carlosr@gmail.com")
+      expect(Company.last.email).to eq("rootnpool@company.com")
     end
 
     scenario "the admin select an user and edit just the email" do
@@ -42,22 +40,22 @@ RSpec.describe "Admin can edit an User", type: :feature do
       sign_in FactoryBot.create(:admin_user)
       visit admin_dashboard_path 
       expect(page).to have_content("Active Admin")
-      click_on("Users")
+      click_on("Companies")
 
-      expect(page).to have_content("elcarlos@gmail.com")
+      expect(page).to have_content("rocknroll@company.com")
       click_on("Editar")
-      expect(page).to have_content("Editar User")
+      expect(page).to have_content("Editar Company")
 
-      within '#edit_user' do
-        fill_in 'user_email', with: 'eldoncarlos@gmail.com'
-        click_on("Guardar User")
+      within '#edit_company' do
+        fill_in 'company_email', with: 'justanotheremail@company.com'
+        click_on("Guardar Company")
       end
 
-      expect(page).to have_content("Detalles de User")
-      expect(page).to have_content("eldoncarlos@gmail.com")
+      expect(page).to have_content("Detalles de Company")
+      expect(page).to have_content("justanotheremail@company.com")
 
-      expect(User.last.email).to eq("eldoncarlos@gmail.com")
-      User.destroy_all
+      expect(Company.last.email).to eq("justanotheremail@company.com")
+      Company.destroy_all
     end
   end
 end

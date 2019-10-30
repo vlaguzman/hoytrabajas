fc90de2b462656913b5baed943bcf7c7a9e368ba
Sebastian Castro <jsebascp6@gmail.com>
2019-10-30 15:45:37 -0500

    add specs

--- spec/features/filter_home_spec.rb
+++ spec/features/filter_home_spec.rb
@@ -1,8 +1,12 @@
 require 'rails_helper'
 
-RSpec.describe "User searches for an offer", type: :feature, js: true do
+RSpec.describe "User searches for an offer", type: :feature do
   context "When user search of offer" do
-    it "Should return offers related" do
+    let!(:offer) { create(:offer, title: "Test sebas") }
+    let!(:offer1) { create(:offer, title: "Esto es un prueba de sebas") }
+    let!(:offer2) { create(:offer, title: "Que gran oferta sebas!") }
+
+    it "Should return offers related", js: true do
       visit root_path
 
       expect(page).to have_content("El empleo ideal para tu tiempo libre")
@@ -11,9 +15,15 @@ RSpec.describe "User searches for an offer", type: :feature, js: true do
         with_tag(:input, with: { name: 'q[title_cont]'})
       end
 
-      save_screenshot('home.jpg')
+      fill_in('q[title_cont]', with: 'sebas')
+      find("button[class='MuiButtonBase-root MuiFab-root mb-10 search_button text-white MuiFab-primary']", visible: false).click
 
-      fill_in("input[name='q[title_cont]']", with: "Test", visible: false)
+      #This expect is done like this because capybara doesn't understand query strings
+      expect(current_path).to eq("#{offers_path}/")
+
+      expect(page).to have_content("Test Sebas")
+      expect(page).to have_content("Esto Es Un Prueba De Sebas")
+      expect(page).to have_content("Que Gran Oferta Sebas!")
     end
   end
 end

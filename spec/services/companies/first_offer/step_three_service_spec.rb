c7999cd47d6b9e59927e0834d9a186389c4554a7
DanielSantiagoCampos <camposdaniel1002@gmail.com>
2019-11-02 16:46:19 -0500

    fix and update manifest

--- spec/services/companies/first_offer/step_three_service_spec.rb
+++ spec/services/companies/first_offer/step_three_service_spec.rb
@@ -12,54 +12,115 @@ RSpec.describe Companies::FirstOffer::StepThreeService do
   it { should be_an_instance_of(Module) }
 
   describe "#call" do
-    context "when all data is correct" do
-      let(:params) do
-        {
-          title: 'Oferta para el mejor desarrollador del mundo mundial',
-          job_category_id: job_category.id,
-          offer_type_id: offer_type.id,
-          work_mode_id: work_mode.id,
-          offers_work_positions: work_position.id
-        }
+
+    describe "when offer exists" do
+      let!(:offer) { create(:offer) }
+      context "when all data is correct" do
+        let(:params) do
+          {
+            id: offer.id,
+            title: 'Oferta para el mejor desarrollador del mundo mundial',
+            job_category_id: job_category.id,
+            offer_type_id: offer_type.id,
+            work_mode_id: work_mode.id,
+            offers_work_positions: work_position.id
+          }
+        end
+
+        it "Should return a new offer" do
+          offer = subject.(company: company, params: params)
+
+          expect(Offer.count).to eq(1)
+
+          expect(offer[:status]).to eq(:ok)
+
+          expect(offer[:data]).to be_an_instance_of(Offer)
+
+          expect(offer[:data].title).to eq(params[:title])
+          expect(offer[:data].job_category_id).to eq(params[:job_category_id])
+          expect(offer[:data].offer_type_id).to eq(params[:offer_type_id])
+
+          expect(offer[:error]).to eq(nil)
+        end
       end
 
-      it "Should return a new offer" do
-        offer = subject.(company: company, params: params)
+      context "when all data is not correct" do
+        let(:params) do
+          {
+            id: offer.id,
+            company_id: '',
+            title: '',
+            job_category_id: job_category.id,
+            offer_type_id: offer_type.id,
+            work_mode_id: work_mode.id,
+            offers_work_positions: work_position.id
+          }
+        end
 
-        expect(Offer.count).to eq(1)
+        it "Should return a errors new offer" do
+          offer = subject.(company: company, params: params)
 
-        expect(offer[:status]).to eq(:ok)
+          expect(Offer.count).to eq(1)
 
-        expect(offer[:data]).to be_an_instance_of(Offer)
+          expect(offer[:status]).to eq(:error)
 
-        expect(offer[:data].description).to eq(params[:description])
+          expect(offer[:data]).to be_an_instance_of(Offer)
 
-        expect(offer[:error]).to eq(nil)
+          expect(offer[:data].errors.details).to eq({:title=>[{:error=>:blank}]})
+        end
       end
     end
 
-    context "when all data is not correct" do
-      let(:params) do
-        {
-          company_id: '',
-          title: '',
-          job_category_id: job_category.description,
-          offer_type_id: offer_type.description,
-          work_mode_id: work_mode.description,
-          offers_work_positions: work_position.description
-        }
+    describe "when offer not exists" do
+      context "when all data is correct" do
+        let(:params) do
+          {
+            title: 'Oferta para el mejor desarrollador del mundo mundial',
+            job_category_id: job_category.id,
+            offer_type_id: offer_type.id,
+            work_mode_id: work_mode.id,
+            offers_work_positions: work_position.id
+          }
+        end
+
+        it "Should return a new offer" do
+          offer = subject.(company: company, params: params)
+
+          expect(Offer.count).to eq(1)
+
+          expect(offer[:status]).to eq(:ok)
+
+          expect(offer[:data]).to be_an_instance_of(Offer)
+
+          expect(offer[:data].description).to eq(params[:description])
+
+          expect(offer[:error]).to eq(nil)
+        end
       end
 
-      it "Should return a errors new offer" do
-        offer = subject.(company: company, params: params)
+      context "when all data is not correct" do
+        let(:params) do
+          {
+            company_id: '',
+            title: '',
+            job_category_id: job_category.id,
+            offer_type_id: offer_type.id,
+            work_mode_id: work_mode.id,
+            offers_work_positions: work_position.id
+          }
+        end
 
-        expect(Offer.count).to eq(0)
+        it "Should return a errors new offer" do
+          offer = subject.(company: company, params: params)
 
-        expect(offer[:status]).to eq(:error)
+          expect(Offer.count).to eq(0)
 
-        expect(offer[:data]).to be_an_instance_of(Offer)
+          expect(offer[:status]).to eq(:error)
 
-        expect(offer[:data].errors.details).to eq({:title=>[{:error=>:blank}], :job_category=>[{:error=>:blank}]})
+          expect(offer[:data]).to be_an_instance_of(Offer)
+
+          expect(offer[:data].errors.details).to eq({:title=>[{:error=>:blank}]})
+        end
       end
     end
   end

979fad1451a285276f5469c37ca9d9f288fa4965
Sebastian Castro <jsebascp6@gmail.com>
2019-11-05 15:22:24 -0500

    offers actives

--- app/services/offers_service.rb
+++ app/services/offers_service.rb
@@ -6,6 +6,7 @@ module OffersService
 
   def self.related_offers_show_details(id=nil, job_category_id=nil)
     Offer
+      .active
       .related_job_category(job_category_id)
       .sample(10)
       .reject { |offer| offer.id.eql?(id) }

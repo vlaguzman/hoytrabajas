6247d2deb41eed9de68ba59c64cb93f92876abc3
Gabriel Meneses <gabrielmeneses9402@gmail.com>
2019-11-06 11:39:20 -0500

    test devise mailers send

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -44,7 +44,7 @@ Rails.application.configure do
    config.action_mailer.smtp_settings = {
      :user_name      => ENV['EMAIL_USER_NAME'],
      :password       => ENV['EMAIL_USER_PASSWORD'],
-     :domain         => ENV['EMAIL_USER_DOMAIN'],
+     :domain         => 'hoytrabajas.com',
      :address        => "smtp.mailgun.org",
      :port           => 587,
      :authentication => :plain

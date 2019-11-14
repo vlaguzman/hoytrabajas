05e4984d2ab2cec520e34ff511633643d46dcd19
Oscar Guzman <oscar.guzman@hoytrabajas.com>
2019-11-14 12:07:44 -0500

    fix restore password user and company

---
+++ spec/features/users/restore_password_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe "User restore password", type: :feature do
+
+  feature "Like a existing user when restore my password" do
+    let(:user) { create(:user) }
+
+    scenario "should visit the restore password page", js: true do
+      visit user_session_path
+
+      click_on '¿Olvidó su contraseña?'
+
+      expect(current_path).to eq(new_user_password_path)
+    end
+  end
+end

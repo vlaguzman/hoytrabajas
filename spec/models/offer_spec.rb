2dff1730bb40710f68c6c227d17e9f1edacad010
Oscar Guzman <oscar.guzman@hoytrabajas.com>
2019-11-20 21:33:06 +0000

    update manifest

--- spec/models/offer_spec.rb
+++ spec/models/offer_spec.rb
@@ -181,8 +181,8 @@ RSpec.describe Offer, type: :model do
       offer_1 = create(:offer, title: 'first created', created_at: Time.new(2018, 01, 01))
       offer_2 = create(:offer, title: 'second created', created_at: Time.new(2019, 01, 02))
 
-      expect(described_class.created_at_desc.first).to eq(offer_2)
-      expect(described_class.created_at_desc.last).to eq(offer_1)
+      expect(described_class.created_at_desc(50).first).to eq(offer_2)
+      expect(described_class.created_at_desc(50).last).to eq(offer_1)
     end
   end
 

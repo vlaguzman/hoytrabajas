fd366f21f925c8b220a3d39a685d4e80c1e9708b
Gabriel Meneses <gabrielmeneses9402@gmail.com>
2019-11-28 09:41:19 -0500

    delete migration, create task for data

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_20_213040) do
+ActiveRecord::Schema.define(version: 2019_11_27_221906) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"

68a0ca3fd8809df6681e1b9f6805bb3e591e1cdf
Javier Segovia <javier.segovia@hoytrabajas.com>
2019-12-27 19:11:04 -0400

    add new footer styles

--- app/javascript/components/src/components/Layout/Footer/Footer.js
+++ app/javascript/components/src/components/Layout/Footer/Footer.js
@@ -1,42 +1,52 @@
-import React, { Fragment, useState, useRef } from 'react'
-import { Collapse, Button, Row } from 'reactstrap'
+import React, { useState, useEffect } from 'react'
+import PropTypes from 'prop-types'
+import { Collapse } from 'reactstrap'
 import UpperFooter from './UpperFooter'
 import UnderFooter from './UnderFooter'
-import SocialBar from './socialBar'
+import SocialBar from './SocialBar'
+import useWindowSize from '../../../hooks/useWindowSize'
 
-const Footer = ({ scrollToBottom, footer_translations, terms_and_conditions_file_path }) => {
-  const [state, setState] = useState({ collapse: true })
+const Footer = ({ footer_translations, terms_and_conditions_file_path }) => {
+  const windowSize = useWindowSize()
+  const [isOpen, setIsOpen] = useState(windowSize.width > 576)
 
-  const scrol = () => {
-    setState({ collapse: !state.collapse })
-    if (state.collapse) {
-      // scrollToBottom()
-    }
-  }
+  const handleToggle = () => setIsOpen(prevState => !prevState)
+
+  useEffect(() => {
+    if (windowSize.width > 576) setIsOpen(true)
+  }, [windowSize])
+
   return (
-    <>
-      <Row noGutters className="position-relative">
-        <Button
-          className="position-absolute d-none"
-          color="primary"
-          onClick={scrol}
-          style={{ left: '50%', marginBottom: '3rem', zIndex: '99' }}
-        >
-          Toggle
-        </Button>
-      </Row>
-      <Collapse isOpen={state.collapse}>
-        <div className="rct-footer d-flex flex-column bg-image animated fadeIn">
-          <SocialBar />
-          <UpperFooter />
-          <UnderFooter
-            underFooterTranslations={footer_translations.under_footer}
-            termAndConditionsPath={terms_and_conditions_file_path}
-          />
+    <footer className="d-flex flex-column animated fadeIn">
+      {windowSize.width <= 576 && (
+        <div>
+          <button
+            type="button"
+            onClick={handleToggle}
+            className="a-button__toggleFooter"
+          >
+            <span className={isOpen ? 'opened' : ''}>
+              {footer_translations.toggle_button}
+            </span>
+          </button>
         </div>
+      )}
+      <Collapse isOpen={isOpen}>
+        <SocialBar translations={footer_translations.social_bar} />
+        <UpperFooter translations={footer_translations.upper_footer} />
+        <UnderFooter
+          windowSize={windowSize}
+          translations={footer_translations.under_footer}
+          termAndConditionsPath={terms_and_conditions_file_path}
+        />
       </Collapse>
-    </>
+    </footer>
   )
 }
 
 export default Footer
+
+Footer.propTypes = {
+  footer_translations: PropTypes.object.isRequired,
+  terms_and_conditions_file_path: PropTypes.string.isRequired
+}
--- app/javascript/components/src/components/Layout/Footer/UnderFooter.js
+++ app/javascript/components/src/components/Layout/Footer/UnderFooter.js
@@ -1,180 +1,81 @@
 import React from 'react'
-import { Row, Col } from 'reactstrap'
-import Typography from '@material-ui/core/Typography'
-import Divider from '@material-ui/core/Divider'
-import AppConfig from '../../../constants/AppConfig'
+import PropTypes from 'prop-types'
 
-const UnderFooter = ({underFooterTranslations={}, termAndConditionsPath=''}) => {
+const defaultPaths = {
+  faq: '/faqs',
+  contact: '/',
+  terms_and_conditions:
+    '/https://ht-web-docs.s3.amazonaws.com/hoytrabajas_tyc.pdf'
+}
+
+const Logo = () => (
+  <div className="underFooter__logo">
+    <img
+      src="/assets/static/img/appLogoW.svg"
+      className="d-block"
+      alt="site logo"
+    />
+  </div>
+)
+
+const UnderFooter = ({ translations, windowSize, paths = defaultPaths }) => {
   return (
-    <div className="row" style={{ backgroundColor: 'rgba(41, 39, 39, 1)' }}>
-      <Row
-        className="px-70 justify-content-between text-white py-25 d-none d-md-flex w-100"
-        style={{ backgroundColor: 'rgba(41, 39, 39, 1)' }}
-      >
-        <Col xs={12} md={6} className="">
-          <Row
-            noGutters
-            className="justify-content-start align-items-center my-20"
-          >
-            <Col xs={5} md={3} className="">
-              <img
-                src="/assets/static/img/appLogoW.svg"
-                className="d-block"
-                alt="site logo"
-                width="100%"
-              />
-            </Col>
-            <Col xs={7} md={9}>
-              <Typography
-                variant="caption"
-                component="span"
-                className="d-inline-block mt-5 ml-5"
-              >
-                Un proyecto 100% Colombiano
-              </Typography>
-            </Col>
-          </Row>
-          <Divider
-            variant="middle"
-            className="d-md-none"
-            style={{ background: 'rgb(93, 90, 90)' }}
-          />
-        </Col>
-        <Col className="">
-          <Row
-            noGutters
-            className="justify-content-end align-items-center h-100"
-          >
-            <Col xs={12} md={2} className="text-center">
-              <Typography variant="body2">
-                <a href="/companies">Empresas</a>
-              </Typography>
-            </Col>
-            <Col xs={12} md={2} className="text-center">
-              <Typography variant="body2">
-                <a href="/job_categories">Categorias de empleo</a>
-              </Typography>
-            </Col>
-            <Col xs={12} md={2} className="text-center">
-              <Typography variant="body2">
-                <a href="/faqs">FAQs</a>
-              </Typography>
-            </Col>
-            <Col xs={12} md={2} className="text-center">
-              <Typography variant="body2">Contacto</Typography>
-            </Col>
-            <Col xs={12} md={4} className="text-center">
-              <a href={termAndConditionsPath} target='blank'>
-                <Typography variant="body2">{underFooterTranslations.terms_and_conditions}</Typography>
-              </a>
-            </Col>
-          </Row>
-        </Col>
-      </Row>
-
-      <Row
-        className="px-40 justify-content-between text-white py-25 d-md-none"
-        style={{ backgroundColor: 'rgba(41, 39, 39, 1)' }}
-      >
-        <Col className="mb-50">
-          <Row noGutters className="justify-content-end h-100">
-            <Col xs={12} md={2} className="align-items-center">
-              <Typography variant="body2">FAQs</Typography>
-            </Col>
-            <Col xs={12} md={2} className="align-items-center">
-              <Typography variant="body2">Contacto</Typography>
-            </Col>
-            <Col xs={12} md={4} className="align-items-center">
-              <a href={termAndConditionsPath} target='blank'>
-                <Typography variant="body2">{underFooterTranslations.terms_and_conditions}</Typography>
-              </a>
-            </Col>
-          </Row>
-        </Col>
-        <Col xs={12} md={6} className="">
-          <Divider
-            variant="middle"
-            className="d-md-none"
-            style={{ background: 'rgb(93, 90, 90)' }}
-          />
-          <Row
-            noGutters
-            className="justify-content-start align-items-center  mt-20 mb-10"
-          >
-            <Col xs={5} md={3} className="">
-              <img
-                src="/assets/static/img/appLogoW.svg"
-                className="d-block"
-                alt="site logo"
-                width="100%"
-              />
-            </Col>
-            <Col xs={7} md={9}>
-              <Typography
-                variant="caption"
-                component="span"
-                className="d-inline-block mt-5"
-              >
-                Un proyecto 100% Colombiano
-              </Typography>
-            </Col>
-          </Row>
-          <div
-            className="d-inline-block text-white text-left w-100"
-            style={{ marginBottom: '2%' }}
-          >
-            {' '}
-            <small className="">Con el apoyo de : </small>{' '}
+    <div className="m-footer__underFooter">
+      <div className="underFooter__wrapper">
+        <div className="underFooter__paths">
+          {windowSize.width > 992 && <Logo />}
+          <ul className="underFooter__navItems">
+            {Object.keys(translations.nav_links).map(
+              pathKey =>
+                paths[pathKey] && (
+                  <li className="li-item">
+                    <a href={paths[pathKey]}>
+                      {translations.nav_links[pathKey]}
+                    </a>
+                  </li>
+                )
+            )}
+          </ul>
+          <div className="underFooter__language">Selecthere</div>
+        </div>
+        <div className="underFooter__divider" />
+        <div className="underFooter__copyright">
+          {windowSize.width <= 992 && <Logo />}
+          <div>
+            <span>{translations.copyright.support_of}</span>
             <img
-              className="rockstart"
-              alt="rockstart"
+              className="underFooter__rockstart"
               src="/assets/static/img/logo_rock.png"
-            />{' '}
+              alt="Rockstart"
+            />
           </div>
-          <div
-            className="d-inline-block text-white text-left w-100"
-            style={{ marginBottom: '5%' }}
-          >
-            <small className="text-muted">
-              Desarrollo: {AppConfig.copyRightText}
-            </small>
+          <div className="underFooter__developedBy">
+            {translations.copyright.developed_by}
+            {windowSize.width > 992 ? (
+              ` - ${translations.copyright.reserved_rights}`
+            ) : (
+              <>
+                <br />
+                {translations.copyright.reserved_rights}
+              </>
+            )}
           </div>
-        </Col>
-      </Row>
-      <Divider
-        variant="middle"
-        className="px-50 mx-auto px-50 w-75 d-none d-md-block"
-        style={{ background: 'rgb(93, 90, 90)' }}
-      />
-      <Col
-        className="align-items-center justify-content-center pcx mt-50 d-none d-md-block"
-        style={{ backgroundColor: 'rgba(41, 39, 39, 1)' }}
-      >
-        <Col className="d-flex flex-column justify-content-center w-100">
-          <div
-            className="d-inline-block text-white text-center w-100"
-            style={{ marginBottom: '2%' }}
-          >
-            {' '}
-            <small className="">Con el apoyo de : </small>{' '}
-            <img
-              className="rockstart"
-              alt="rockstart"
-              src="/assets/static/img/logo_rock.png"
-            />{' '}
-          </div>
-          <div
-            className="d-inline-block text-white text-center w-100"
-            style={{ marginBottom: '5%' }}
-          >
-            <small className="text-muted">
-              Desarrollo: {AppConfig.copyRightText}
-            </small>
-          </div>
-        </Col>
-      </Col>
+        </div>
+      </div>
     </div>
   )
 }
 
 export default UnderFooter
+
+UnderFooter.propTypes = {
+  translations: PropTypes.shape({
+    nav_links: PropTypes.object.isRequired,
+    copyright: PropTypes.object.isRequired
+  }).isRequired,
+  paths: PropTypes.shape({
+    faq: PropTypes.string.isRequired,
+    contact: PropTypes.string.isRequired,
+    terms_and_conditions: PropTypes.string.isRequired
+  }).isRequired
+}

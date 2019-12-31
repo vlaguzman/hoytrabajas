c956c7d2423c407b50503b9088fea0cee10af53c
Oscar Guzman <oscar.guzman@hoytrabajas.com>
2019-12-31 00:26:05 -0500

    add registration errors and spanish translations

--- app/controllers/companies/registrations_controller.rb
+++ app/controllers/companies/registrations_controller.rb
@@ -1,7 +1,5 @@
 # frozen_string_literal: true
 class Companies::RegistrationsController < Devise::RegistrationsController
-  before_action :authenticate_company!
-
   protected
 
   def after_inactive_sign_up_path_for(resource)

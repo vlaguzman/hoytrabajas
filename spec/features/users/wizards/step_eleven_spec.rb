6e24b5d21ed05a7963bb90d0d8e305e0edc7b57d
Javier Segovia <javier.segovia@hoytrabajas.com>
2020-01-19 22:15:38 -0400

    Run bin/shot

--- spec/features/users/wizards/step_eleven_spec.rb
+++ spec/features/users/wizards/step_eleven_spec.rb
@@ -1,13 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe "like new candidate", :type => :feature do
+  let(:curriculum_vitae) do
+    create(:curriculum_vitae, :empty, user: create(:user, :first_time_candidate))
+  end
+
+  let(:candidate) { curriculum_vitae.user }
+
   feature "When click on 'Completar mi perfil'" do
-    let(:curriculum_vitae) do
-        create(:curriculum_vitae, :empty, user: create(:user, :first_time_candidate))
-    end
-
-    let(:candidate) { curriculum_vitae.user }
-
     scenario "Should redirect to 'users/dashboard'", js: true do
       sign_in candidate
 

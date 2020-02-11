23d74ea1b4d3ec7e8df5063d2a8e74e0ce4ff03b
Gabriel Meneses <gabrielmeneses9402@gmail.com>
2020-02-11 09:50:36 -0500

    changes in react companent

--- app/javascript/components/UserUploadPhoto.js
+++ app/javascript/components/UserUploadPhoto.js
@@ -1,32 +1,44 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import PhotoCamera from '@material-ui/icons/PhotoCamera';
+import CheckIcon from '@material-ui/icons/Check';
 
 const UserUploadPhoto = props => {
   { action, method, csrf_param, csrf_token } = props;
 
-  const onSubmitChange = (e) {
-    e.preventDefault();
-  }
+  const [defaultButton, setDefaultButton] = useState(false)
 
   const handleOnChange = (e) {
-    e.target.value
-    //aqui es cuando debo ejecutar el submit
-  }
+    let attach_file = e.target.files
+    attach_file.length >= 1 && setDefaultButton(true)
+  };
 
   return (
     <form action={action} method="post">
       <input type="hidden" name={csrf_param} value={csrf_token}> </input>
       <input type="hidden" name="_method" value={method}> </input>
 
-      <input accept="image/*" className={classes.input} id="icon-button-file" type="file" />
-      <label htmlFor="icon-button-file">
-        <IconButton color="primary" aria-label="upload picture" component="span">
-          <PhotoCamera />
-        </IconButton>
-      </label>
-      <input type="submit"> </input>
+      { defaultButton ?
+
+        (<>
+          <input accept="image/*" id="icon-button-file" type="file" />
+          <label htmlFor="icon-button-file">
+            <IconButton color="primary" aria-label="upload picture" component="span">
+              <PhotoCamera />
+            </IconButton>
+          </label>
+        </>)
+      :
+        (<>
+           <input id="icon-button-file" type="submit" />
+           <label htmlFor="icon-button-file">
+             <IconButton color="primary" aria-label="upload picture" component="span">
+               <CheckIcon />
+             </IconButton>
+           </label>
+         </>)
+      }
     </form>
   )
 }

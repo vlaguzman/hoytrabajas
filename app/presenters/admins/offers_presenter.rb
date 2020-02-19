22a90b77a9a9ed31b09f3484980f9278337ee144
Gabriel Meneses <gabrielmeneses9402@gmail.com>
2020-02-19 15:02:27 -0500

    fix problem in admin view

--- app/presenters/admins/offers_presenter.rb
+++ app/presenters/admins/offers_presenter.rb
@@ -9,7 +9,8 @@ class Admins::OffersPresenter < ApplicationPresenter
   end
 
   def get_collection_by_model(model)
-    model.all.pluck(:id, :description)
+    model.all.pluck(:id, :description).map { |value| [ value[0], value[1].truncate(30) ] }
+
   end
 
   def get_error_by_model(model, attribute)

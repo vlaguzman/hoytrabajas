b153d06a28f7fbaa49414f2879ad481f89ccee28
Gabriel Meneses <gabrielmeneses9402@gmail.com>
2020-03-03 13:39:50 -0500

    bin/shot

--- spec/features/home/index/unregistered_candidate_click_in_applied_offer_spec.rb
+++ spec/features/home/index/unregistered_candidate_click_in_applied_offer_spec.rb
@@ -110,5 +110,167 @@ RSpec.describe "Unregistered user applied offer in home", type: :feature do
     expect(current_path).to eq(offer_path(offer.id))
     expect(page).to have_text("¡HAS APLICADO!")
     end
+
+    it "Should skip add educational level and arrive at the offer show", js: true do
+
+    visit root_path
+
+    expect(page).to have_text("Tremendous Offer")
+    find(".cardOffer", match: :first).hover
+    has_css?('.btn-apply')
+    find(".btn-apply", match: :first).click
+
+    expect(current_path).to eq(new_user_registration_path)
+
+    expect(page).to have_text("¡Hola! Regístrate ahora")
+    fill_in 'user[email]',                 :with => "kendrick.test@email.com"
+    fill_in 'user[password]',              :with => "holamundo101"
+    fill_in 'user[password_confirmation]', :with => "holamundo101"
+
+    find('.termsAndConditions__checkbox').click
+
+    has_button?("Registrarme")
+    find(".a-button", text: "Registrarme", visible: false).click
+
+    expect(current_path).to eq(users_wizards_step_zero_path)
+
+    expect(page).to have_text("Quiero completar mi perfil")
+    click_on('Quiero completar mi perfil')
+
+    expect(current_path).to eq(users_wizards_step_zeros_curriculum_vitae_path)
+
+    expect(page).to have_text("No tengo hoja de vida")
+    click_on("No tengo hoja de vida")
+
+    expect(current_path).to eq(users_wizards_step_one_path)
+
+    fill_in 'user[name]', with: 'Carlos'
+    fill_in 'user[last_name]', with: 'Rojas'
+
+    find("div[id='mui-component-select-user[born_country_id]", visible: false).click
+    find("li", text: "Argentina").click
+
+    find("div[id='mui-component-select-user[born_state_id]", visible: false).click
+    find("li", text: "Buenos Aires").click
+
+    find("div[id='mui-component-select-user[born_city_id]", visible: false).click
+    find("li", text: "Capital Federal").click
+
+    find("div[id='mui-component-select-user[residence_country_id]", visible: false).click
+    find("li", text: "Colombia").click
+
+    find("div[id='mui-component-select-user[residence_state_id]", visible: false).click
+    find("li", text: "Huila").click
+
+    find("div[id='mui-component-select-user[residence_city_id]", visible: false).click
+    find("li", text: "Neiva").click
+
+    find("div[id='mui-component-select-user[nationality_ids][]']", visible: false).click
+    find("li", text: "Argentina").click
+    find("div[id='mui-component-select-user[nationality_ids][]']", visible: false).click
+    find("li", text: "Colombiana").click
+
+    find("div[id='mui-component-select-user[document_type_id]']", visible: false).click
+    find("li", text: "Cedula de Ciudadania").click
+
+    fill_in "user[identification_number]", :with => "1063558224"
+    fill_in "user[contact_number]", :with => "3183638789"
+
+    execute_script "window.scrollTo(0, (window.innerHeight * 2) )"
+    find("span", text: /SIGUIENTE/).click
+
+    expect(current_path).to eq(users_wizards_step_two_path)
+
+    visit users_wizards_step_nines_choices_path
+
+    expect(current_path).to eq(users_wizards_step_nines_choices_path)
+
+    expect(page).to have_text("Omitir")
+    click_on("Omitir")
+
+    expect(current_path).to eq(offer_path(offer.id))
+    expect(page).to have_text("¡HAS APLICADO!")
+    end
+
+    it "Should skip add educational level and arrive at the offer show", js: true do
+
+    visit root_path
+
+    expect(page).to have_text("Tremendous Offer")
+    find(".cardOffer", match: :first).hover
+    has_css?('.btn-apply')
+    find(".btn-apply", match: :first).click
+
+    expect(current_path).to eq(new_user_registration_path)
+
+    expect(page).to have_text("¡Hola! Regístrate ahora")
+    fill_in 'user[email]',                 :with => "kendrick.test@email.com"
+    fill_in 'user[password]',              :with => "holamundo101"
+    fill_in 'user[password_confirmation]', :with => "holamundo101"
+
+     find('.termsAndConditions__checkbox').click
+
+    has_button?("Registrarme")
+    find(".a-button", text: "Registrarme", visible: false).click
+
+    expect(current_path).to eq(users_wizards_step_zero_path)
+
+    expect(page).to have_text("Quiero completar mi perfil")
+    click_on('Quiero completar mi perfil')
+
+    expect(current_path).to eq(users_wizards_step_zeros_curriculum_vitae_path)
+
+    expect(page).to have_text("No tengo hoja de vida")
+    click_on("No tengo hoja de vida")
+
+    expect(current_path).to eq(users_wizards_step_one_path)
+
+    fill_in 'user[name]', with: 'Carlos'
+    fill_in 'user[last_name]', with: 'Rojas'
+
+    find("div[id='mui-component-select-user[born_country_id]", visible: false).click
+    find("li", text: "Argentina").click
+
+    find("div[id='mui-component-select-user[born_state_id]", visible: false).click
+    find("li", text: "Buenos Aires").click
+
+    find("div[id='mui-component-select-user[born_city_id]", visible: false).click
+    find("li", text: "Capital Federal").click
+
+    find("div[id='mui-component-select-user[residence_country_id]", visible: false).click
+    find("li", text: "Colombia").click
+
+    find("div[id='mui-component-select-user[residence_state_id]", visible: false).click
+    find("li", text: "Huila").click
+
+    find("div[id='mui-component-select-user[residence_city_id]", visible: false).click
+    find("li", text: "Neiva").click
+
+    find("div[id='mui-component-select-user[nationality_ids][]']", visible: false).click
+    find("li", text: "Argentina").click
+    find("div[id='mui-component-select-user[nationality_ids][]']", visible: false).click
+    find("li", text: "Colombiana").click
+
+    find("div[id='mui-component-select-user[document_type_id]']", visible: false).click
+    find("li", text: "Cedula de Ciudadania").click
+
+    fill_in "user[identification_number]", :with => "1063558224"
+    fill_in "user[contact_number]", :with => "3183638789"
+
+    execute_script "window.scrollTo(0, (window.innerHeight * 2) )"
+    find("span", text: /SIGUIENTE/).click
+
+    expect(current_path).to eq(users_wizards_step_two_path)
+
+    visit users_wizards_step_nines_choices_path
+
+    expect(current_path).to eq(users_wizards_step_nines_choices_path)
+
+    expect(page).to have_text("No, pero quiero aprender")
+    click_on("No, pero quiero aprender")
+
+    expect(current_path).to eq(offer_path(offer.id))
+    expect(page).to have_text("¡HAS APLICADO!")
+    end
   end
 end

2ee493de87a87d635fdff2d98c37a68e34e64dfe
Vladimir Guzman Mendoza <guzman.vla@gmail.com>
2020-03-05 17:27:48 -0500

    add methods to save and modify data

--- app/services/clientify/data_creator.rb
+++ app/services/clientify/data_creator.rb
@@ -4,9 +4,10 @@ require "net/http"
 class Clientify::DataCreator
 
   URL_CONTACTS = "https://api.clientify.net/v1/contacts/"
+  URL_EDIT_CONTACT = "https://api.clientify.net/v1/contacts/7005475/"
   STATUS = 'usuario pag'
 
-  def create_user u
+  def create_candidate u
 
     url = URI(URL_CONTACTS)
 
@@ -15,12 +16,55 @@ class Clientify::DataCreator
 
     clientify = Clientify::ApiAuth.new
     request = Net::HTTP::Post.new(url)
-    request["Authorization"] = "Token #{clientify.token}"
+    request["Authorization"] = "Token #{clientify.obtain_token}"
     request["Content-Type"] = "application/json"
-    request.body = "{\n    \"first_name\": \"#{u.name}\",\n    \"last_name\": \"#{u.last_name}\",\n    \"email\": \"#{u.email}\",\n    \"phone\": \"#{u.contact_number}\",\n    \"status\": \"#{STATUS}\",\n    \"contact_source\": \hoytrabajas.com\",\n    \"addresses\": [{\"street\":\"\", \"city\":\"\", \"state\":\"\", \"country\":\"\", \"postal_code\":\"\", \"type\":1}],\n    \"custom_fields\": [],\n    \"description\": \"Sunt vitae consequun\",\n    \"remarks\": \"Consequatur aliquid\",\n    \"summary\": \"Voluptas dolorem com\",\n    \"message\": \"Nobis aliquip quia c\",\n    \"re_property_name\": \"Hakeem Hicks\",\n    \"last_contact\": null\n}"
+    request.body = "{\n    \"email\": \"#{u.email}\",
+                     \n    \"tags\": [\"candidate\", \"htweb\"]
+                     }"
 
     response = https.request(request)
     puts response.read_body
   end 
+
+  def create_employer u
+
+    url = URI(URL_CONTACTS)
+
+    https = Net::HTTP.new(url.host, url.port);
+    https.use_ssl = true
+
+    clientify = Clientify::ApiAuth.new
+    request = Net::HTTP::Post.new(url)
+    request["Authorization"] = "Token #{clientify.obtain_token}"
+    request["Content-Type"] = "application/json"
+    request.body = "{\n    \"first_name\": \"#{u.name}\",
+                     \n    \"last_name\": \"#{u.last_name}\",
+                     \n    \"email\": \"#{u.email}\",
+                     \n    \"phone\": \"#{u.contact_number}\",
+                     \n    \"tags\": [\"employer\", \"htweb\"],
+                     \n    \"status\": \"#{STATUS}\"}"
+
+    response = https.request(request)
+    puts response.read_body
+  end
+
+  def update_user u
+    url = URI(URL_EDIT_CONTACT)
+
+    https = Net::HTTP.new(url.host, url.port);
+    https.use_ssl = true
+
+    clientify = Clientify::ApiAuth.new
+    request = Net::HTTP::Put.new(url)
+    request["Authorization"] = "Token #{clientify.obtain_token}"
+    request["Content-Type"] = "application/json"
+    request.body = "{\n    \"first_name\": \"#{u.name}\",
+                     \n    \"last_name\": \"#{u.last_name}\",
+                     \n    \"phone\": \"#{u.contact_number}\"
+                     }"
+
+    response = https.request(request)
+    puts response.read_body
+  end
 
 end

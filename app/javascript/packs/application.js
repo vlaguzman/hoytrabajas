3e903dd917bd184df05c6bec9cccdd8bbf8cf2d1
Javier Segovia <javier.segovia@hoytrabajas.com>
2020-03-20 11:17:04 -0400

    Optimize webpack SplitChunks

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,4 +1,4 @@
-/* eslint no-console:0 */
+/* eslint-disable */
 // This file is automatically compiled by Webpack, along with any other files
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
@@ -7,7 +7,6 @@
 // To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
 // layout file, like app/views/layouts/application.html.erb
 
-
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
@@ -15,20 +14,18 @@
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
 
-import "regenerator-runtime/runtime";
-
 // Support component names relative to this directory:
-var componentRequireContext = require.context("components", true);
-var ReactRailsUJS = require("react_ujs");
-ReactRailsUJS.useContext(componentRequireContext);
+import 'regenerator-runtime/runtime'
 
-//use only required icons
-import { library } from '@fortawesome/fontawesome-svg-core';
-import * as Icons from '@fortawesome/free-solid-svg-icons';
+const componentRequireContext = require.context('components', true)
+const ReactRailsUJS = require('react_ujs')
+ReactRailsUJS.useContext(componentRequireContext)
 
-const iconList = Object
-  .keys(Icons)
-  .filter(key => key !== "fas" && key !== "prefix" )
+import { library } from '@fortawesome/fontawesome-svg-core'
+import * as Icons from '@fortawesome/free-solid-svg-icons'
+
+const iconList = Object.keys(Icons)
+  .filter(key => key !== 'fas' && key !== 'prefix')
   .map(icon => Icons[icon])
 
 library.add(...iconList)
--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,26 +1,50 @@
-const path = require('path');
+const path = require('path')
 const webpack = require('webpack')
 const { environment } = require('@rails/webpacker')
-const typescript =  require('./loaders/typescript')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const TerserPlugin = require('terser-webpack-plugin')
+const typescript = require('./loaders/typescript')
 
 environment.plugins.prepend(
   'MomentContextReplacement',
   new webpack.ContextReplacementPlugin(/moment[\/\\]locale$/, /en|es/)
 )
 
+environment.plugins.prepend('CleanWebpackPlugin', new CleanWebpackPlugin())
+
 environment.config.merge({
-  module:{
-    rules:[
+  module: {
+    rules: [
       {
         test: /\.jsx?$/,
-        loader: "babel-loader",
-        exclude: [
-          path.resolve(__dirname, "node_modules")
-        ]
+        loader: 'babel-loader',
+        exclude: [path.resolve(__dirname, 'node_modules')]
       }
     ]
   }
 })
+
+environment.splitChunks(config => ({
+  ...config,
+  optimization: {
+    minimizer: [
+      new TerserPlugin({
+        cache: true
+      })
+    ],
+    runtimeChunk: 'single',
+    splitChunks: {
+      chunks: 'async',
+      maxSize: 600000,
+      cacheGroups: {
+        defaultVendors: {
+          test: /[\\/]node_modules[\\/]/,
+          reuseExistingChunk: true
+        }
+      }
+    }
+  }
+}))
 
 environment.loaders.prepend('typescript', typescript)
 module.exports = environment

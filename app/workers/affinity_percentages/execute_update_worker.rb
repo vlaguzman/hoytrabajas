72073b91189d9a62ae40600e396330b8ef648ee3
Oscar Guzman <oscar.guzman@hoytrabajas.com>
2020-03-30 14:52:44 -0500

    bad comment

--- app/workers/affinity_percentages/execute_update_worker.rb
+++ app/workers/affinity_percentages/execute_update_worker.rb
@@ -1,10 +1,10 @@
-#class AffinityPercentages::ExecuteUpdateWorker
-#  include Sidekiq::Worker
-#
-#  sidekiq_options queue: :affinitypercentage, retry: false
-#
-#  def perform(curriculum_vitae_id)
-#    AffinityPercentages::ExecuteWorkerService.(curriculum_vitae_id)
-#  end
-#
-#end
+class AffinityPercentages::ExecuteUpdateWorker
+  include Sidekiq::Worker
+
+  sidekiq_options queue: :affinitypercentage, retry: false
+
+  def perform(curriculum_vitae_id)
+    #AffinityPercentages::ExecuteWorkerService.(curriculum_vitae_id)
+  end
+
+end

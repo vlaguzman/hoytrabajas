d88550384364b010098ca3b790405da7034be4ac
Oscar Guzman <oscar.guzman@hoytrabajas.com>
2020-04-07 12:15:43 -0500

    remove the new analitics

--- app/javascript/packs/analytics/a_b_testing.js
+++
@@ -1,5 +0,0 @@
-window.dataLayer = window.dataLayer || [];
-function gtag(){dataLayer.push(arguments);}
-gtag('js', new Date());
-
-gtag('config', 'UA-108065726-1', { 'optimize_id': 'GTM-WL3BQVS'});
